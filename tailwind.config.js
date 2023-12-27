70c906935f8dd9046dbf2b5256744c5948c33336
sayfiddingithub5799 <sayfiddingithub5799@gmail.com>
2023-12-27 15:58:09 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      height: {
+        boyi: '600px'
+      },
+      backgroundColor: {
+        rang: 'red'
+      },
+    },
+  },
+  plugins: [],
+}
